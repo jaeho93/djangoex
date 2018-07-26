7721dda6e917c4820a993cbe246f2190a05f6656
Jaeho Cho <jaeho8032@gmail.com>
2018-07-26 23:45:05 +0900

    blog.models.post 모델에 기본정렬 지정

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -28,6 +28,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+    class Meta:
+        ordering = ['-id']
     
     def __str__(self):
         return self.title
