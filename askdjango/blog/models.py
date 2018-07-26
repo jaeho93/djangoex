8a1de60bb6e1de11f8bce25537a4405bf5a8c77f
Jaeho Cho <jaeho8032@gmail.com>
2018-07-26 23:09:29 +0900

    blog.views.post_list 뷰에서 포스터 목록 구현

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,7 +1,7 @@
+# blog/models.py
 import re
 from django.db import models
 from django.forms import ValidationError
-from django.utils import timezone
 
 
 def lnglat_validator(value):
@@ -10,12 +10,24 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, verbose_name='제목'   ) # 길이 제한이 있다.
-    content = models.TextField(help_text='Markdown 문법을 써주세요.', verbose_name='내용')             # 길이 제한이 없다.
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
+
+    author = models.CharField(max_length=20)
+    title = models.CharField(max_length=100, verbose_name='제목',
+        help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')  # 길이 제한이 있는 문자열
+    content = models.TextField(verbose_name='내용')              # 길이 제한이 없는 문자열
     tags = models.CharField(max_length=100, blank=True)
-    lnglat = models.CharField(max_length=50,
-        blank=True,
+    lnglat = models.CharField(max_length=50, blank=True,
         validators=[lnglat_validator],
-        help_text='경도,위도 포맷으로 입력',)
-    created_at = models.DateTimeField(default=timezone.now)
+        help_text='경도/위도 포맷으로 입력')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    
+    def __str__(self):
+        return self.title
