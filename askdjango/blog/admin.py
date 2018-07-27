4525736d8f7be6048f15b72dcfe2a450a0632acd
Jaeho Cho <jaeho8032@gmail.com>
2018-07-27 11:50:08 +0900

    blog.models.Comment 모델 생성/admin 등록

--- askdjango/blog/admin.py
+++ askdjango/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -10,3 +10,7 @@ class PostAdmin(admin.ModelAdmin):
         return '{}글자'.format(len(post.content))
 
 # admin.site.register(Post, PostAdmin)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Post
 
 def post_list(request):
     qs = Post.objects.all()
+
 
     q = request.GET.get('q', '')
     if q:
@@ -11,4 +13,11 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
         'q': q,
+    })
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
     })
