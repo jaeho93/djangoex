c34225c83dffe430626af3ee76d92e7b7c798b18
Jaeho Cho <jaeho8032@gmail.com>
2018-08-03 11:06:56 +0900

    blog.models.Post get_absolute_url 구현

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -38,6 +38,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post)
